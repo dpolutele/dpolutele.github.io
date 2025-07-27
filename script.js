21478fce5726b646c0de66efe266c96abd6b5f4e
dpolutele <dylangiovannipolutele@gmail.com>
2025-07-28 10:56:58 +1100

    maj du site principal

--- script.js
+++ script.js
@@ -22,54 +22,62 @@ O.prototype = {
         this.color = 'hsl(180, 100%, 50%)';
         this.w = 2;
         this.h = 1;
-        // Vitesse ralentie sur mobile
-        this.vy = isMobile ? random(1.5, 2.5) : random(4, 5);
+        this.vy = isMobile ? random(2.5, 3.5) : random(4, 5);
         this.vw = 3;
         this.vh = 1;
-        // Taille ajustée selon l'appareil
         this.size = isMobile ? 1.2 : 2;
-        this.hit = random(h * .8, h * .9);
+        this.hit = random(h * 0.8, h * 0.9);
         this.a = 1;
-        this.va = .96;
+        this.va = 0.96;
+        this.isSplashing = false;  // nouveau flag
     },
     draw: function() {
         if (this.y > this.hit) {
-            ctx.beginPath();
-            ctx.moveTo(this.x, this.y - this.h / 2);
-
-            ctx.bezierCurveTo(
-                this.x + this.w / 2, this.y - this.h / 2,
-                this.x + this.w / 2, this.y + this.h / 2,
-                this.x, this.y + this.h / 2);
-
-            ctx.bezierCurveTo(
-                this.x - this.w / 2, this.y + this.h / 2,
-                this.x - this.w / 2, this.y - this.h / 2,
-                this.x, this.y - this.h / 2);
-
-            ctx.strokeStyle = 'hsla(180, 100%, 50%, ' + this.a + ')';
-            ctx.stroke();
-            ctx.closePath();
-
+            if (!isMobile) {
+                // Splash uniquement sur PC
+                ctx.beginPath();
+                ctx.moveTo(this.x, this.y - this.h / 2);
+                ctx.bezierCurveTo(
+                    this.x + this.w / 2, this.y - this.h / 2,
+                    this.x + this.w / 2, this.y + this.h / 2,
+                    this.x, this.y + this.h / 2
+                );
+                ctx.bezierCurveTo(
+                    this.x - this.w / 2, this.y + this.h / 2,
+                    this.x - this.w / 2, this.y - this.h / 2,
+                    this.x, this.y - this.h / 2
+                );
+                ctx.strokeStyle = 'hsla(180, 100%, 50%, ' + this.a + ')';
+                ctx.stroke();
+                ctx.closePath();
+            } else {
+                // Sur mobile, on dessine rien si la goutte est à l’impact
+                // Juste on ne dessine pas de splash
+            }
         } else {
             ctx.fillStyle = this.color;
             ctx.fillRect(this.x, this.y, this.size, this.size * (isMobile ? 3 : 5));
         }
-        this.update();
     },
     update: function() {
         if (this.y < this.hit) {
             this.y += this.vy;
         } else {
-            if (this.a > .03) {
-                this.w += this.vw;
-                this.h += this.vh;
-                if (this.w > 100) {
-                    this.a *= this.va;
-                    this.vw *= .98;
-                    this.vh *= .98;
+            if (!isMobile) {
+                // Splash animation uniquement PC
+                if (this.a > 0.03) {
+                    this.w += this.vw;
+                    this.h += this.vh;
+                    if (this.w > 100) {
+                        this.a *= this.va;
+                        this.vw *= 0.98;
+                        this.vh *= 0.98;
+                    }
+                } else {
+                    this.init();
                 }
             } else {
+                // Sur mobile, réinitialiser directement à l’impact, pas de splash
                 this.init();
             }
         }
@@ -77,9 +85,7 @@ O.prototype = {
 }
 
 function resize() {
-    // Désactivé pour garder une taille fixe
-    // w = c.width = window.innerWidth;
-    // h = c.height = window.innerHeight;
+    // fix size volontaire
 }
 
 function setup() {
@@ -89,8 +95,8 @@ function setup() {
                 var o = new O();
                 o.init();
                 drops.push(o);
-            }, j * 200)
-        }(i));
+            }, j * 200);
+        })(i);
     }
 }
 
@@ -99,6 +105,7 @@ function anim() {
     ctx.fillRect(0, 0, w, h);
     for (var i in drops) {
         drops[i].draw();
+        drops[i].update();
     }
     requestAnimationFrame(anim);
 }
